9772fd601f82fdc3f790e65449cfcda2cc64715e
petrero <petrero@gmail.com>
2018-02-01 19:29:16 +0200

    And yep, you can see the GuardAuthenticationHandler class in the debug:autowiring list. But... what if it weren't there? What if we were trying to autowire a service that was not in this list?
        Well... you would get a huge error. And maybe you should ask yourself: is this not a service I'm supposed to be using?
    
        But anyways, if it's not in the list, there's a simple solution: go to services.yml and add your own alias. At the bottom, paste the class you want to use as the type-hint, then copy the service id, and say @ and paste.
    
        Yep, that is all you need to do in order to define your own autowiring rules. Since we don't need it in this case, comment it out.
    
        Ok, refresh! At this point, our goal is to hunt for deprecations until we're pretty confident they're gone: it's not an exact science. If you have a test suite, you can use the symfony/phpunit-bridge(https://symfony.com/doc/current/components/phpunit_bridge.html) to get a report of deprecated code paths that are hit in your tests.
    
    There is one more deprecation on the registration page. Look at the details:
    
    Call Form::isValid() on an unsubmitted form is deprecated. Use Form::isSubmitted() before Form::isValid().

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
         $form = $this->createForm(UserRegistrationForm::class);
 
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isValid()) {
             /** @var User $user */
             $user = $form->getData();
             $em = $this->getDoctrine()->getManager();
