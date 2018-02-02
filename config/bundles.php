c91dabb8020e43f938839a3da34a312535a7a744
petrero <petrero@gmail.com>
2018-02-02 20:20:57 +0200

    Scaffold the Full Structure Ok, that was a huge step. Run composer update:
    composer update
    The biggest change is that we're not relying on symfony/symfony anymore. And, yep! You can see it remove symfony/symfony and start adding individual libraries.
    
    Ah, it explodes! Don't worry about that yet: Composer did finish and Flex configured 3 new recipes!
    
    At this point, the new Flex project is fully built... and it already works! I'll prove it to you next.

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 ];
