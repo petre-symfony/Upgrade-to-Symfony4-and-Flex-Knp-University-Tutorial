48ebc7a03772a2fd87d51416dff8fae93ffc6a00
petrero <petrero@gmail.com>
2018-02-03 14:37:18 +0200

    Copy code from branch 13

---
+++ src/Controller/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function homepageAction()
+    {
+        return $this->render('main/homepage.html.twig');
+    }
+}
