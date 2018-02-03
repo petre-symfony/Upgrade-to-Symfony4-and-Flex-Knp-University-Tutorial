5bf74808810aaab41362120c4963da123bed384c
petrero <petrero@gmail.com>
2018-02-03 10:33:44 +0200

    Previously onAuthenticationSuccess method was handled by the base class for you. But now, it's your responsibility. No worries: it's pretty simple. To help, at the top, use a trait called TargetPathTrait. Back down in onAuthenticationSuccess, this allows us to say if {dollar_sign}targetPath = {dollar_sign}this->getTargetPath() with {dollar_sign}request->getSession() and main.
    Let's break this down. First, the main string is just the name of our firewall. In both the old and new security config, that's its key.
    
    Second, what does getTargetPath() do? Well, suppose the user originally tried to go to /admin, and then they were redirected to the login page. After they login, we should probably send them back to /admin, right? The getTargetPath() method returns the URL that the user originally tried to access, if any.
    
    So if there is a target path, return new RedirectResponse({dollar_sign}targetPath). Else, return new RedirectResponse and generate a URL to the homepage.
    
    PhpStorm thinks this isn't a real route, but it is!
    
    Problem solved! Is that enough to make our app happy? Find out!
    
    ./bin/console
    It is!

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -12,50 +12,50 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
-{
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator {
+
+    use TargetPathTrait;
+
     private $formFactory;
     private $em;
     private $router;
     private $passwordEncoder;
 
-    public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router, UserPasswordEncoderInterface $passwordEncoder)
-    {
+    public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router, UserPasswordEncoderInterface $passwordEncoder) {
         $this->formFactory = $formFactory;
         $this->em = $em;
         $this->router = $router;
         $this->passwordEncoder = $passwordEncoder;
     }
-    
-    public function supports(Request $request){
-      return $request->getPathInfo() == '/login' && $request->isMethod('POST');   
+
+    public function supports(Request $request) {
+        return $request->getPathInfo() == '/login' && $request->isMethod('POST');
     }
 
-    public function getCredentials(Request $request)
-    {
+    public function getCredentials(Request $request) {
         $form = $this->formFactory->create(LoginForm::class);
         $form->handleRequest($request);
 
         $data = $form->getData();
         $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $data['_username']
+                Security::LAST_USERNAME, $data['_username']
         );
 
         return $data;
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider)
-    {
+    public function getUser($credentials, UserProviderInterface $userProvider) {
         $username = $credentials['_username'];
 
         return $this->em->getRepository('AppBundle:User')
-            ->findOneBy(['email' => $username]);
+                        ->findOneBy(['email' => $username]);
     }
 
-    public function checkCredentials($credentials, UserInterface $user)
-    {
+    public function checkCredentials($credentials, UserInterface $user) {
         $password = $credentials['_password'];
 
         if ($this->passwordEncoder->isPasswordValid($user, $password)) {
@@ -65,13 +65,16 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         return false;
     }
 
-    protected function getLoginUrl()
-    {
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey) {
+        if ($targetPath = $this->getTargetPath($request->getSession(), 'main')){
+            return new RedirectResponse($targetPath);
+        };
+        
+        return new RedirectResponse($this->router->generate('homepage'));
+    }
+
+    protected function getLoginUrl() {
         return $this->router->generate('security_login');
     }
 
-    protected function getDefaultSuccessRedirectUrl()
-    {
-        return $this->router->generate('homepage');
-    }
 }
