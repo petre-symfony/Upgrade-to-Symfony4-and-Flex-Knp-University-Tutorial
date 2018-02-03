56345724202d084d4d20fa85a143583e73e9b4f3
petrero <petrero@gmail.com>
2018-02-03 10:18:35 +0200

    composer require security It downloads and then... another error! Interesting:
    LoginFormAuthenticator contains 1 abstract method
    
    Ah! I think we missed a deprecation warning, and now we're seeing a fatal error. Open AppBundle/Security/LoginFormAuthenticator.php.
    
    PhpStorm agrees: class must implement method onAuthenticationSuccess.

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
