63ed004c8cbbe84074d2de394a78e56f10ba02f9
petrero <petrero@gmail.com>
2018-02-03 13:45:35 +0200

    If you want to stop now, you can! Your old code lives in src/AppBundl…
    …e, but it works! Over time, you can slowly migrate it directly into src/.
    
    Or! We can keep going: take this final challenge head-on and move all our files at once! If you're not using PhpStorm... this will be a nightmare. Yep, this is one of those rare times when you really need to use it.
    
    Moving your Files
    Open AppBundle.php. Then, right click on the AppBundle namespace and go to Refactor -> Move. The new namespace will be App. And below... yea! The target destination should be src/.
    
    This says: change all AppBundle namespaces to App and move things into the src/ directory. Try it! On the big summary, click OK!
    
    In addition to changing the namespace at the top of each file, PhpStorm is also searching for references to the namespaces and changing those too. Will it be perfect? Of course not! But that last pieces are pretty easy.
    
    Woh! Yes! Everything is directly in src/. AppBundle is now empty, except for a fixtures.yml file. We're going to replace that file soon anyways.
    
    Delete AppBundle! That felt amazing!
    
    Refactoring tests/
    Let's do the same thing for the tests/ directory... even though we only have one file. Open DefaultControllerTest.php and Refactor -> Move its namespace. In Flex, the namespace should start with App\Tests. Then, press F2 to change the directory to tests/Controller.
    Ok, Refactor! Nice! Now delete that AppBundle.

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Security;
+
+use AppBundle\Form\LoginForm;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator {
+
+    use TargetPathTrait;
+
+    private $formFactory;
+    private $em;
+    private $router;
+    private $passwordEncoder;
+
+    public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router, UserPasswordEncoderInterface $passwordEncoder) {
+        $this->formFactory = $formFactory;
+        $this->em = $em;
+        $this->router = $router;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function supports(Request $request) {
+        return $request->getPathInfo() == '/login' && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request) {
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+
+        $data = $form->getData();
+        $request->getSession()->set(
+                Security::LAST_USERNAME, $data['_username']
+        );
+
+        return $data;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider) {
+        $username = $credentials['_username'];
+
+        return $this->em->getRepository('AppBundle:User')
+                        ->findOneBy(['email' => $username]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user) {
+        $password = $credentials['_password'];
+
+        if ($this->passwordEncoder->isPasswordValid($user, $password)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey) {
+        if ($targetPath = $this->getTargetPath($request->getSession(), 'main')){
+            return new RedirectResponse($targetPath);
+        };
+        
+        return new RedirectResponse($this->router->generate('homepage'));
+    }
+
+    protected function getLoginUrl() {
+        return $this->router->generate('security_login');
+    }
+
+}
